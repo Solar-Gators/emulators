802743982037e9cd97255879f381b043e9d92e8a
John Carr <johnjcarr17@gmail.com>
2020-07-02 18:17:52 -0400

    Folder structure complete

--- DAD/DAD.py
+++ DAD/DAD.py
@@ -1,32 +1,32 @@
-from ctypes import *
+import ctypes
 import math
 import sys
 import time
-from './UART'
+from UART import UART
 
 class DAD():
     def __init__(self, com_type):
         if sys.platform.startswith("win"):
-            self.dwf = cdll.LoadLibrary("dwf.dll")
+            self.dwf = ctypes.cdll.LoadLibrary("dwf.dll")
         elif sys.platform.startswith("darwin"):
-            self.dwf = cdll.LoadLibrary("/Library/Frameworks/dwf.framework/dwf")
+            self.dwf = ctypes.cdll.LoadLibrary("/Library/Frameworks/dwf.framework/dwf")
         else:
-            self.dwf = cdll.LoadLibrary("libdwf.so")
+            self.dwf = ctypes.cdll.LoadLibrary("libdwf.so")
 
-        hdwf = c_int()
+        hdwf = ctypes.c_int()
 
         print("Opening first device")
         #dwf.FDwfDeviceOpen(c_int(-1), byref(hdwf))
         # device configuration of index 3 (4th) for Analog Discovery has 16kS digital-in/out buffer
-        self.dwf.FDwfDeviceConfigOpen(c_int(-1), c_int(3), byref(hdwf))
+        self.dwf.FDwfDeviceConfigOpen(ctypes.c_int(-1), ctypes.c_int(3), ctypes.byref(hdwf))
         if hdwf.value == 0:
             print("failed to open device")
-            szerr = create_string_buffer(512)
+            szerr = ctypes.create_string_buffer(512)
             self.dwf.FDwfGetLastErrorMsg(szerr)
             print(str(szerr.value))
             quit()
         if com_type == "UART":
-            self.protocol = UART()
+            self.protocol = UART(ctypes.byref(self.dwf), ctypes.byref(hdwf))
         elif com_type == "CAN":
             pass
         elif com_type == "SPI":
@@ -36,10 +36,7 @@ class DAD():
         else:
             print("invalid comunication protocol.")
             quit()
-    # Return received data
-    def receive(self):
-        if self.protocol == "UART":
-            pass
-        elif self.protocol == "CAN":
-            dwf.FDwfDigitalCanRx(hdwf, byref(vID), byref(fExtended), byref(fRemote), byref(cDLC), rgbRX, c_int(sizeof(rgbRX)), byref(vStatus))
-        
+    def sendData(self, data, size):
+        self.protocol.send(data, size)
+    def receiveData(self):
+        return self.protocol.receive()
--- pythonC_examples.py
+++ pythonC_examples.py
@@ -1,11 +1,11 @@
 from ctypes import *
 import random
-import Orion
+from Mocks import Orion
 # items must be declared in the order they should be received
 # index 0 ... index n
 
 
-OrionBMS = Orion.OrionData(0x12)
+OrionBMS = Orion.OrionData(0x12, 0x01)
 arr, size = OrionBMS.toCharArray_c()
 print(arr)
 print(size)
