c3900308a17dd7de28926615c13ef9a672ab1754
John Carr <johnjcarr17@gmail.com>
2020-07-05 20:11:21 -0400

    Added mitsuba frame fields

---
+++ Mocks/Mitsuba.py
@@ -0,0 +1,28 @@
+from .Message import Message
+
+class Mitsuba(Message):
+    def __init__(self, addr_CAN, addr_telem):
+        super().__init__(addr_CAN, addr_telem)
+        # Frame 0 data
+        self.battVoltage = 12       # 10 bit 0.5v LSB
+        self.battCurrent = 11       #  9 bit 1A/LSB
+        self.battCurrentDir = 10    #  1 bit '0' (+) / '1' (-)
+        self.motorCurrent = 10      # 10 bit 1A/LSB 
+        self.FETtemp = 9            #  5 bit 5C/LSB
+        self.motorRspeed = 8        # 12 bit 1rpm/LSB
+        self.duty = 10              # 10 bit 0.5%/LSB
+        self.advancedLeadAngle = 13 #  7 bit 0.5Deg_e/LSB
+        # Frame 1 data
+        self.mode = 1               #  1 bit '0' (eco) / '1' (power)
+        self.control = 1            #  1 bit Current Control Mode PWM Mode
+        self.accelPos = 10          # 10 bit 0.5%/LSB
+        self.regenPos = 10          # 10 bit 0.5%/LSB
+        self.DigiSWNum = 4          #  4 bit Digi SW Number
+        self.targetVal = 10         # 10 bit
+        self.motorStat = 2          #  2 bit Wait/Forward/Reverse
+        self.drive = 1              #  1 bit'0' (Drive) / '1' (Regen)
+        # Frame 2 data
+        self.ADSensorErr = 21       # 16 bit
+        self.PowSysErr = 8          #  8 bit
+        self.MotorSysErr = 3        #  8 bit
+        self.FETOHLvl = 2           #  2 bit
